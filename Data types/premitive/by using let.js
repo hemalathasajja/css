5db9a8d772b53278bafb7074a385de75eb716e0c
hema-sasi <sajja.hemalathasasi@gmail.com>
2024-11-06 08:43:52 +0530

    ok

---
+++ Data types/premitive/by using let.js
@@ -0,0 +1,10 @@
+let a=10;
+let b="javascript";
+var c=true;
+console.log(a);
+console.log(b);
+console.log(a+b);
+console.log(c);
+console.log(a+b+c);
+
+//there is no difference using let instead of var
